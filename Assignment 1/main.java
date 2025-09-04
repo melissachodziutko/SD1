d1f986c3b68e8a04dc45c6b8650aa5ea6cf1ab9e
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2025-09-04 13:25:48 -0400

    Update main.java

--- Assignment 1/main.java
+++ Assignment 1/main.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+      Scanner sc = new Scanner (System.in);
+      System.out.println("What is your major?");
+      String major = sc.nextLine();
+      System.out.println("What is your school?");
+      String school = sc.nextLine();
+      System.out.println("What year are you?");
+      String year = sc.nextLine();
+      System.out.println("You go to " + school + ", you're majoring in " + major + ", and you're a " + year);
+      
+
+
+
+    }
+}
 
